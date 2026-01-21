99e863ff61cc1451438018feac82cd9c6c1e797f
sadaksamo <sadaksamor99@gmail.com>
2026-01-21 14:54:11 +0300

    Completed Nav

--- client/src/Components/Nav.jsx
+++ client/src/Components/Nav.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { MenuIcon }  from 'lucide-react'
 
 const Nav = () => {
     const [open,setOpen] = useState(false)
+    const [currentUser] = useState({name:"farax"})
+    const navigate = useNavigate()
   return (
     <div>
         <div className='hidden md:flex justify-between items-center bg-gray-700 text-white px-20 py-2'>
@@ -13,16 +15,30 @@ const Nav = () => {
                 <Link to={'/'}> About</Link>
                 <Link to={'/'}> Contact</Link>
             </div>
+            {
+                !currentUser ? 
             <div className='bg-blue-600 px-2 py-1 text-white'>
                 Login
+            </div> :
+            <div onClick={()=>navigate("/profile")} className='w-8 h-8 rounded-full bg-black text-white font-bold text-center flex flex-col justify-center items-center'>
+                {currentUser?.name.slice(0,1).toUpperCase()}
             </div>
+            }
         </div>
 
 
         {/* //mobile */}
         <div className='md:hidden flex justify-between items-center bg-gray-700 text-white px-8 py-2'>
             <h1>RealState</h1>
+            <div className='flex space-x-5'>
+                {
+            currentUser &&
+            <div onClick={()=>navigate("/profile")} className='w-8 h-8 rounded-full bg-black text-white font-bold text-center flex flex-col justify-center items-center'>
+                {currentUser?.name.slice(0,1).toUpperCase()}
+            </div>
+            }
             <button onClick={()=>setOpen(!open)}><MenuIcon/></button>
+            </div>
         </div>
         {
             open &&
@@ -32,9 +48,15 @@ const Nav = () => {
                 <Link to={'/'}> About</Link>
                 <Link to={'/'}> Contact</Link>
             </div>
+            {
+                currentUser  ? 
+            <div className='bg-red-600 px-2 py-1 text-white'>
+                Logout
+            </div>:
             <div className='bg-blue-600 px-2 py-1 text-white'>
                 Login
             </div>
+            }
             </div>
         }
     </div>
