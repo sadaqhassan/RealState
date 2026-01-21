f1a22cdd5a9b5bc56a3bf410f172d3dd4c1a2fa6
sadaksamo <sadaksamor99@gmail.com>
2026-01-21 14:43:44 +0300

    Rautes and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import Nav from './Components/Nav'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Auth from './Pages/Auth'
+import ProfilePage from './Pages/ProfilePage'
 
 const App = () => {
   return (
     <div>
       <Nav/>
+
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/auth' element={<Auth/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/profile' element={<ProfilePage/>}/>
+      </Routes>
+
     </div>
   )
 }
